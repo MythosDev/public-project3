c0b644edbfcff697a92e99dc73972f5064c07d92
MythosDev <buschyboy14@gmail.com>
2025-02-04 01:41:56 -0800

    added an image to the about section

--- vite-project/src/pages/About.jsx
+++ vite-project/src/pages/About.jsx
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom';
 import MovieRow from '../components/MovieRow';
+import demoPic from '../assets/mypic.png';
+
 import CSharp from 'programming-languages-logos/src/csharp/csharp.png';
 import Javascript from 'programming-languages-logos/src/javascript/javascript.png';
 import Java from 'programming-languages-logos/src/java/java.png';
@@ -19,7 +21,25 @@ export default function () {
         linker: {
             fontSize: "36px",
             fontWeight: "800"
-        }
+        },
+
+        imageContainer: {
+            width: "100%", // Full width
+            height: '500px', // ADJUST FOR IMAGE SHOWN
+            maxWidth: '500px',
+            overflow: "hidden", // Ensures cropping of the image
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            margin: '0 auto',
+        },
+        img: {
+            width: "100%", // Make the image scale to fill width
+            height: "100%", // Ensures it fills the container height
+            objectFit: "cover", // Ensures it maintains aspect ratio while cropping top and bottom
+            objectPosition: 'center center', //ADJUST FOR WHERE IMAGE GETS CROPPED
+        
+        },
     }
 
     const customMovies = [
@@ -39,17 +59,41 @@ export default function () {
         <>
             <div>
                 <Link to="/Home" style={styles.linker}>Home</Link>
+                <br />
+                <br />
+                <div style={styles.imageContainer}>
+                    <img src={demoPic} alt="" style={styles.img}/>
+                </div>
                 <h2>About Me</h2>
                 <p>My name is <b>William Busch</b></p>
-                <p>I am from ___</p>
-                <p>I have a passion for ___</p>
-                <p>
-                    I enjoy filmmaking, <b>European</b> art, Game Design <br></br>
-                    I have a particular interest in silent films <br></br>
-                    My passion has always been for <b>cinematography</b> <br></br>
-                    I enjoy good art and good design which has led me here <br></br>
-                    I also have an interest in procedural generation and maps 
-                </p>
+                <p>I am from Vancouver British Columbia</p>
+                <div style={{width: '500px', margin: 'auto', textAlign: 'justify'}}>
+                    <p>
+                        I have always had a passion for art.
+                        Designing websites and posters is something I like to do in my free time.
+                        The pyschology of consumer descisions fascinates me.
+                        As does the technology behind products.
+                    </p>
+                    <p>
+                        I have a passion for maps. 
+                        Which has led me to procedural generation.
+                        I enjoy creating simulations.
+                        Creating life through algorithms is one of my joys in life.
+                        Not as much of a fan of AI though.
+                    </p>
+                    <p>
+                        I enjoy filmmaking, particularly cinematography and want to work on short films in the future.
+                        My fascination is mostly concentrated to fantasy films and silent films.
+                        Creating beautiful images that transport people to different worlds and capture memories is my goal in life.
+                    </p>
+                    <p>
+                        Video Games are also a passion of mine. Although not playing them. I enjoy Making them.
+                        I have been designing and creating video games for 7 years and have no plans to stop.
+                        My focus is primarily on coop games and procedurally generated games.
+                        The joy from playing games with your friends is something that is often forgotten in the industry today, 
+                        and I believe is something that should be brought back.
+                    </p>
+                </div>
                 <h2>Languages</h2>
                 <MovieRow movies={customMovies}/>
                 <h2>Programs</h2>
